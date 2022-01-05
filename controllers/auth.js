1b70ab1c6f36ae8c9037eab97d53f87cbb37c2bb
ErickSODE <ericksode@gmail.com>
2022-01-05 13:12:40 -0600

    Loggin with Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { response, request } = require('express');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res=response) => {
     const {correo, password} = req.body;
@@ -42,6 +44,46 @@ const login = async(req, res=response) => {
 
 }
 
+const googleSignIn = async(req = request, res = response) => {
+    const { id_token } = req.body;
+    try {
+        const {nombre, img, correo} = await googleVerify(id_token);
+        let usuario = await Usuario.findOne({correo});
+        //Si el usuario no existe lo crea con la data de google
+        if(!usuario){
+            const data = {
+                nombre,
+                correo,
+                password: ':O',
+                img,
+                google: true
+            }
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+        //Si el usuario existe pero su estado es inactivo
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el admin, usuario inactivo'
+            });
+        }
+
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });   
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se  puedo verificar - Google SignIn'
+        });
+    }
+};
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
